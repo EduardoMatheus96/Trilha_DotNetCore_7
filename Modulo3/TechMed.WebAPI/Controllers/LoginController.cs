14609b8f4090466cbb2ef4cdcae0a0d719c5236e
eduardomatheus96 <eduardomatheusuesc@gmail.com>
2024-02-08 14:56:58 -0300

    Creating LoginController

---
+++ Modulo3/TechMed.WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using TechMed.Application.InputModels;
+
+namespace TechMed.WebAPI.Controllers;
+
+[ApiController]
+[Route("/api/v0.1/")]
+public class LoginController : ControllerBase
+{
+    public IActionResult Login([FromBody] LoginInputModel user){
+        throw new NotImplementedException();
+    }
+}
