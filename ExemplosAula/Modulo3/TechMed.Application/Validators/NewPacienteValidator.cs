ac7ee2898009a0a9f6c9c0b04c90277d735c08c4
eduardomatheus96 <eduardomatheusuesc@gmail.com>
2024-02-09 16:10:43 -0300

    Creating RUles for Pacient

--- ExemplosAula/Modulo3/TechMed.Application/Validators/NewPacienteValidator.cs
+++ ExemplosAula/Modulo3/TechMed.Application/Validators/NewPacienteValidator.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using FluentValidation;
 using TechMed.Application.InputModels;
 
@@ -11,5 +12,20 @@ public class NewPacienteValidator : AbstractValidator<NewPacienteInputModel>
                 .WithMessage("O nome do paciente é obrigatorio")
             .MinimumLength(3)
                 .WithMessage("O nome do paciente deve ter no minimo três caracteres");
+        RuleFor(p => p.Cpf)
+            .NotEmpty()
+                .WithMessage("O CPF do paciente é obrigatorio.")
+            .Must(ValidateCpf)
+                .WithMessage("O CPF deve estar no formato 000.000.000-00.");
+        RuleFor(p => p.Email)
+            .EmailAddress().WithMessage("O e-mail do paciente é invalido");
+    }
+    public static bool ValidateCpf(string cpf)
+    {
+        string cpfPattern = @"^\d{3}\.\d{3}\.\d{3}-\d{2}$";
+
+        Regex regex = new(cpfPattern);
+
+        return regex.IsMatch(cpf);
     }
 }
