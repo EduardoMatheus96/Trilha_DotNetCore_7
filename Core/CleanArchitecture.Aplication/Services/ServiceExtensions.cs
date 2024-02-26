ad241d079dc32efe2ddd92f85a529f17497bd540
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2024-02-26 15:20:26 -0300

    Att

---
+++ Core/CleanArchitecture.Aplication/Services/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Application.Services;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureApplicationApp(this IServiceCollection services)
+    {
+        
+    }
+}
